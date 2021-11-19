9aa06f4e248bfd814876e183f4062c1611e945de
pedroajs <pedroajs93@gmail.com>
2021-11-19 16:11:17 -0300

    Alguns dos metodos do controller

--- src/main/kotlin/com/learningkt/bankapi/AccountContoller.kt
+++ src/main/kotlin/com/learningkt/bankapi/AccountContoller.kt
@@ -1,11 +1,21 @@
 package com.learningkt.bankapi
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/accounts")
 
 class AccountContoller(private val repository: AccountRepository) {
+    @PostMapping
+    fun create(@RequestBody account: Account):Account = repository.save(account)
 
+    @GetMapping
+    fun getAll(): List<Account> = repository.findAll()
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): ResponseEntity<Account> =
+            repository.findById(id).map{
+                ResponseEntity.ok(it)
+            }.orElse(ResponseEntity.notFound().build())
 }
