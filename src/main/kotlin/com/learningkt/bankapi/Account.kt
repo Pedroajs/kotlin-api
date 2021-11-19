e45e2cea97ddc9c18a2b24af6739d7566a5c51ea
pedroajs <pedroajs93@gmail.com>
2021-11-19 15:58:06 -0300

    criando uma classe entidade account

---
+++ src/main/kotlin/com/learningkt/bankapi/Account.kt
@@ -0,0 +1,17 @@
+package com.learningkt.bankapi
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+// Entity informa que uma classe é uma entidade e que seus objetos devem ser persistidos no db
+// O JPA obriga que toda classe entity tenha um ID
+@Entity(name ="accounts")
+data class Account(
+        @Id @GeneratedValue
+         var id: Long? = null,
+         val name: String,
+         val document: String,
+         val phone: String
+
+)
