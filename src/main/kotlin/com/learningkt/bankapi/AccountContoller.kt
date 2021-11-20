59bdc6432c6f0bb742d08017652a44250045ea75
pedroajs <pedroajs93@gmail.com>
2021-11-20 14:56:38 -0300

    ultimos metodos

--- src/main/kotlin/com/learningkt/bankapi/AccountContoller.kt
+++ src/main/kotlin/com/learningkt/bankapi/AccountContoller.kt
@@ -1,5 +1,6 @@
 package com.learningkt.bankapi
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -17,5 +18,22 @@ class AccountContoller(private val repository: AccountRepository) {
     fun getById(@PathVariable id: Long): ResponseEntity<Account> =
             repository.findById(id).map{
                 ResponseEntity.ok(it)
+            }.orElse(ResponseEntity.notFound().build())
+
+    @PutMapping("/{id}")
+    fun upDate(@PathVariable id: Long, @RequestBody account: Account): ResponseEntity<Account> =
+            repository.findById(id).map{
+                val accountUpDate = it.copy(
+                        name = account.name,
+                        document = account.document,
+                        phone = account.phone
+                )
+                ResponseEntity.ok(repository.save(accountUpDate))
+            }.orElse(ResponseEntity.notFound().build())
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long): ResponseEntity<Void> =
+            repository.findById(id).map{
+                repository.delete(it)
+                ResponseEntity<Void>(HttpStatus.OK)
             }.orElse(ResponseEntity.notFound().build())
 }
