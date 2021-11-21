ea2a2c88baa22be4496e19ea05a57445ae8610f0
pedroajs <pedroajs93@gmail.com>
2021-11-21 17:46:45 -0300

    Criando testes, primeiro metodo findAll com testes para tipagem

---
+++ src/test/kotlin/com/learningkt/bankapi/AccountControllerTest.kt
@@ -0,0 +1,33 @@
+package com.learningkt.bankapi
+
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired lateinit var mockMvc: MockMvc
+    @Autowired lateinit var accountRepository: AccountRepository
+
+
+    @Test
+    fun testFindAll(){
+        accountRepository.save(Account(name = "Test", document = "123", phone = "98764310" ))
+        mockMvc.perform(MockMvcRequestBuilders.get("/accounts"))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("\$").isArray)
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[0].id").isNumber)
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[0].name").isString)
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[0].document").isString)
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[0].phone").isString)
+                .andDo(MockMvcResultHandlers.print())
+    }
+}
